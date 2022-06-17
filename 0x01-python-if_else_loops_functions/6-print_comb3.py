f3ee65c9b1c668d0af5347ebaad8e165c38dbbcb
UgonmaC <madgy.clems@gmail.com>
2022-06-17 08:05:50 +0000

    errors fixed

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+for tens in range(0, 9):
+    for units in range(tens + 1, 10):
+        if (tens == 8 and units == 9):
+            print('{:d}{:d}'.format(tens, units))
+        else:
+            print('{:d}{:d},'.format(tens, units), end=' ')
---
+++ 0x01-python-if_else_loops_functions/README.md
@@ -0,0 +1,1 @@
+This is a project of Python if/else statements
